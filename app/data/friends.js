7295d51330e97807f49c64447f4c86bdcbd63a10
Flippen, Ben <benflippen@gmail.com>
2019-07-27 21:25:09 -0500

    done

---
+++ app/data/friends.js
@@ -0,0 +1,51 @@
+var friendList = [{
+        name: 'Mordecai',
+        photo: 'https://vignette.wikia.nocookie.net/theregularshow/images/8/89/Latest-3.png/revision/latest?cb=20190612191655',
+        scores: [
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5'
+        ]
+    },
+    {
+        name: 'Rigby',
+        photo: 'https://vignette.wikia.nocookie.net/theregularshow/images/d/dd/Rigby.png/revision/latest?cb=20190612191840',
+        scores: [
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            1,
+            1,
+            1,
+        ]
+    },
+    {
+        name: 'Muscle Man',
+        photo: 'https://vignette.wikia.nocookie.net/theregularshow/images/1/1e/Muscle_hq.png/revision/latest?cb=20190612191629',
+        scores: [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+        ]
+    }
+];
+
+module.exports = friendList;
